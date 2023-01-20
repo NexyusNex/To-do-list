2aaee1e9bda547218f33258d4446e14f11ec840f
NexyusNex <shootdietale@gmail.com>
2023-01-20 21:31:11 +0100

    Renamed dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = {
     bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name].js",
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, "docs"),
     },
     port: 3000,
     open: true,
